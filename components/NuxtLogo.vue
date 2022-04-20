ee99b62c0fd1dd61e52f06999ee9d656f68b1736
Jordan Momper <jordan.momper.simplon@gmail.com>
2022-04-20 09:33:31 +0200

    initial commit

---
+++ components/NuxtLogo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit quia ea,
+      illum a architecto excepturi quas nostrum eveniet quis. Totam eum nemo
+      culpa fugit nam dolorem ipsa ut nobis ipsam.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NuxtLogo",
+};
+</script>
+
+<style scoped>
+.po {
+  height: 180px;
+}
+</style>
---
+++ components/Tutorial.vue
@@ -0,0 +1,22 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit quia ea,
+      illum a architecto excepturi quas nostrum eveniet quis. Totam eum nemo
+      culpa fugit nam dolorem ipsa ut nobis ipsam.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NuxtTutorial",
+};
+</script>
+
+<style scoped>
+p {
+  color: red;
+}
+</style>
